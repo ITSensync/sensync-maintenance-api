ac6920e87c7eb644ea3469b44a8af0b315ffdc56
Alief Muhammad Abdillah <aliefmabdillah09@gmail.com>
2026-02-06 17:46:47 +0700

    refactor: header type

--- src/controller/generate.controller.js
+++ src/controller/generate.controller.js
@@ -4,7 +4,7 @@ async function generateKorektif(req, res) {
   const { buffer, filename } = await generateService.BAKorektif(req.body);
 
   res.setHeader("Content-Type", "application/pdf");
-  res.setHeader("Content-Disposition", `attachment; filename="${filename}"`);
+  res.setHeader("Content-Disposition", `inline; filename="${filename}"`);
   res.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
 
   res.send(buffer);
